3c1cd707b940f0af0c3911adccde339976290510
Alan Leverenz <awleverenz@aol.com>
2020-06-18 21:45:02 -0400

    Add Contact Route

--- routes/contacts.js
+++ routes/contacts.js
@@ -24,9 +24,36 @@ router.get('/', auth, async (req, res) => {
 // @route   POST api/contacts
 // @desc    Add new contact
 // @access  Private
-router.post('/', (req, res) => {
-  res.send('Add contact');
-});
+router.post(
+  '/',
+  [auth, [check('name', 'Name is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, company, note, type } = req.body;
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        company,
+        note,
+        type,
+        user: req.user.id,
+      });
+      // save contact to database
+      const contact = await newContact.save();
+      // return contact to client
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route   PUT api/contacts/:id
 // @desc    Update contact
