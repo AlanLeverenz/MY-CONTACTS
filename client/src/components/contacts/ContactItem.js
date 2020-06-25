dd45f50560364d2ae072ad569f1622d0867095a7
Alan Leverenz <awleverenz@aol.com>
2020-06-25 16:47:31 -0400

    added dateFormat

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,18 +1,20 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import ContactContext from '../../context/contact/contactContext';
-
+import dateFormat from 'dateformat';
 const ContactItem = ({ contact }) => {
   const contactContext = useContext(ContactContext);
 
   const { deleteContact, setCurrent, clearCurrent } = contactContext;
 
-  const { _id, name, email, phone, type, company, note } = contact;
+  const { _id, name, email, phone, type, company, note, date } = contact;
 
   const onDelete = () => {
     deleteContact(_id);
     clearCurrent();
   };
+
+  const dateFormatted = dateFormat([date], 'mmmm dS, yyyy');
 
   return (
     <div className='card bg-light'>
@@ -55,6 +57,15 @@ const ContactItem = ({ contact }) => {
           <li>
             <i className='fas fa-comment-alt' style={{ color: 'darkorange' }} />{' '}
             {note}
+          </li>
+        )}
+        {date && (
+          <li>
+            <i
+              className='fas fa-calendar-day'
+              style={{ color: 'darkorchid' }}
+            />{' '}
+            {dateFormatted}
           </li>
         )}
       </ul>
